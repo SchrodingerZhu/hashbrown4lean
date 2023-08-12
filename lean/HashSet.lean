7a2eabfcf8cd07f214d1e07609427601f27c8e9b
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-08-12 13:10:17 -0400

    convert lean symbol to caml cases

--- lean/HashSet.lean
+++ lean/HashSet.lean
@@ -10,30 +10,30 @@ instance : Nonempty (HashSetIter α) := (HashSetIterPointed α).property
 
 -- Initialization routines
 @[extern "lean_hashbrown_register_hashset_class"]
-private opaque register_hashset_class : IO PUnit
+private opaque registerHashSetClass : IO PUnit
 
 @[extern "lean_hashbrown_register_hashset_iter_class"]
-private opaque register_hashset_iter_class : IO PUnit
+private opaque registerHashSetIterClass : IO PUnit
 
 @[init]
-private def init_module : IO Unit := do 
- register_hashset_class
- register_hashset_iter_class
+private def initModule : IO Unit := do 
+ registerHashSetClass
+ registerHashSetIterClass
 
 -- APIs
 @[extern "lean_hashbrown_hashset_create"]
 opaque HashSet.mk : {α : Type} → HashSet α
 
 @[extern "lean_hashbrown_hashset_insert"]
-private opaque HashSet.insert_raw : {α : Type} 
+private opaque HashSet.insertRaw : {α : Type} 
   → HashSet α → UInt64 → α → @&(α → Bool) → @&(α → UInt64) → HashSet α 
 
 @[extern "lean_hashbrown_hashset_contains"]
-private opaque HashSet.contains_raw : {α : Type} 
+private opaque HashSet.containsRaw : {α : Type} 
   → @& HashSet α → UInt64 → @&(α → Bool) → Bool
 
 @[extern "lean_hashbrown_hashset_remove"]
-private opaque HashSet.remove_raw : {α : Type} 
+private opaque HashSet.removeRaw : {α : Type} 
   → HashSet α → UInt64 → @&(α → Bool) → HashSet α
 
 @[extern "lean_hashbrown_hashset_len"]
@@ -55,17 +55,17 @@ def HashSet.insert {α : Type} [Hashable α] [BEq α] (s: HashSet α) (a: α) : 
   let hash := Hashable.hash a
   let eq := fun (b: α) => a == b
   let hasher := Hashable.hash
-  HashSet.insert_raw s hash a eq hasher
+  HashSet.insertRaw s hash a eq hasher
 
 def HashSet.remove {α : Type} [Hashable α] [BEq α] (s: HashSet α) (a: α) : HashSet α :=
   let hash := Hashable.hash a
   let eq := fun (b: α) => a == b
-  HashSet.remove_raw s hash eq  
+  HashSet.removeRaw s hash eq  
 
 def HashSet.contains {α : Type} [Hashable α] [BEq α] (s: @& HashSet α) (a: α) : Bool :=
   let hash := Hashable.hash a
   let eq := fun (b: α) => a == b
-  HashSet.contains_raw s hash eq
+  HashSet.containsRaw s hash eq
 
 private partial def formatTail [Repr α] [Nonempty α] (acc: Std.Format) (level: Nat) (tail: HashSetIter α) : Std.Format :=
   if tail.hasElement then
