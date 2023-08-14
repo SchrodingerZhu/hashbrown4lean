f5d52e26cb594b95b5c65b0209923b967efbcef0
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-08-14 16:55:09 -0400

    support IDE

--- lakefile.lean
+++ lakefile.lean
@@ -24,7 +24,7 @@ lean_exe «swisstable» {
 
 extern_lib liblean_hashbrown pkg := do
   let name := nameToStaticLib "lean_hashbrown"
-  let libFile := pkg.dir / "target" / "release" / name
+  let libFile := pkg.buildDir / "lib" / name
   let cargoFile ← inputFile <| pkg.dir / "Cargo.toml"
   let librsFile ← inputFile <| pkg.dir / "src" / "lib.rs"
   let setFile ← inputFile <| pkg.dir / "src" / "set.rs"
@@ -32,5 +32,6 @@ extern_lib liblean_hashbrown pkg := do
   let ffiFile ← inputFile <| pkg.dir / "src" / "ffi.rs"
   buildFileAfterDepArray libFile #[cargoFile, librsFile, setFile, mapFile, ffiFile] (fun _ => proc {
     cmd := "cargo",
-    args := #["build", "--release"]
-  } true) (pure BuildTrace.nil) 
+    args := #["build", "--release", "-Zunstable-options", "--target-dir", (pkg.buildDir / "rust").toString, "--out-dir", (pkg.buildDir / "lib").toString]
+  } true) (pure BuildTrace.nil)
+  
